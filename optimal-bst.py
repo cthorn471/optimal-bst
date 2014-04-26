622e848460bf52c27f05fa9e620f5684d1e47d3b
Thomas Trebat <tjtrebat@gmail.com>
2014-04-26 17:39:05 -0300

    Added argparse

--- optimal-bst.py
+++ optimal-bst.py
@@ -1,9 +1,17 @@
 __author__ = 'Tom'
 
 class OptimalBST:
-    def __init__(self):
-        pass
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+
+    
 
 if __name__ == "__main__":
-    bst = OptimalBST()
+    import argparse
+    parser = argparse.ArgumentParser(description='Find the optimal binary search tree from the probabilities of keys')
+    parser.add_argument('--p', metavar='P', type=float, nargs='+', help='A list of probabilities for each key')
+    parser.add_argument('--q', metavar='Q', type=float, nargs='+', help='A list of probabilities for each dummy key')
+    args = parser.parse_args()
+    bst = OptimalBST(args.p, args.q)
 
